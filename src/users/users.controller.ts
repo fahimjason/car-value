2e365f1f72df552cd59095ca1a6fb340a9b154de
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2022-11-23 19:11:14 +0600

    Create and save a user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from './dtos/create-user.dto'
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+    constructor(private userService: UsersService) {
+
+    }
+
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
-        console.log(body);
+        this.userService.create(body.email, body.password);
     }
 }
