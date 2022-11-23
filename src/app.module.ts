3e9faa5afa29f64ef56569f447804cfb10abb62d
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2022-11-23 15:35:45 +0600

    Create entity and repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/reports.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User, Report],
       synchronize: true,
     }),
     UsersModule,
