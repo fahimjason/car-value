999aa116ea5ed3362fd088965e441af9c1cb5e9e
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2022-11-28 13:07:30 +0600

    Implement controller-wize serialization in user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,7 +8,6 @@ import {
     Param,
     Query,
     NotFoundException,
-    UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -17,6 +16,7 @@ import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private userService: UsersService) { }
 
@@ -25,8 +25,6 @@ export class UsersController {
         this.userService.create(body.email, body.password);
     }
 
-    // @UseInterceptors(new SerializeInterceptors(UserDto))
-    @Serialize(UserDto)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         const user = await this.userService.findOne(parseInt(id));
