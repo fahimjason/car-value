0a5dd033b659906660a76207f22802f6ec416736
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2022-11-30 12:17:10 +0600

    Create custom decorators

---
+++ src/users/decorators/current-user.decorator.ts
@@ -0,0 +1,5 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const CurrentUser = createParamDecorator((data: any, context: ExecutionContext) => {
+    return 'Hi'
+})
