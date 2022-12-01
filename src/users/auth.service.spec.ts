1c01c521ef4c54b913c7ae847ba9c8040f8cb042
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2022-12-01 22:45:33 +0600

    Get Typescript to help with mocks and make speed up tests

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 it('can create an instance of auth service', async () => {
     // Create a fake copy of user service
-    const fakeUserService = {
+    const fakeUserService: Partial<UsersService> = {
         find: () => Promise.resolve([]),
-        create: (email: string, password: string) => Promise.resolve({ id: 1, email, password })
+        create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User)
     }
 
     const module = await Test.createTestingModule({
