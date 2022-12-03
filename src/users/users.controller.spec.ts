1db9e35c4795d8abb7fb2bffc78911a63e13b67c
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2022-12-03 18:14:00 +0600

    Test signin method in user.controller.spec

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -24,7 +24,9 @@ describe('UsersController', () => {
 
     fakeAuthService = {
       // signup: () => { },
-      // signin: () => { }
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User)
+      }
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -65,5 +67,14 @@ describe('UsersController', () => {
     fakeUserService.findOne = () => null;
 
     await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+
+    const user = await controller.signin({ email: 'user@gmail.com', password: 'user' }, session);
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
