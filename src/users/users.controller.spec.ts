e5b992c955b16f80c3db7eb60a14a15ccd2eb08f
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2022-12-03 15:24:08 +0600

    Implement more mocks for user controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { NotFoundException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -45,5 +46,24 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('user@gmail.com');
+
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('user@gmail.com');
+  });
+
+  it('findUser returns a single user with a given id', async () => {
+    const user = await controller.findUser('1');
+
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id not found', async () => {
+    fakeUserService.findOne = () => null;
+
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
   });
 });
