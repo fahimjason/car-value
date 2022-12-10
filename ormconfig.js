05394ad1b2f7b3912f101765cd54db036d6f6478
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2022-12-10 23:27:26 +0600

    Remove ormconfig.json

--- ormconfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: process.env.Node_ENV == 'development' ? ['**/*.entity.js'] : ['**/*.entity.ts'],
-    synchronize: false,
-};
--- src/app.module.ts
+++ src/app.module.ts
@@ -16,24 +16,17 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`
     }),
-    TypeOrmModule.forRoot(),
-    // TypeOrmModule.forRootAsync({
-    //   inject: [ConfigService],
-    //   useFactory: (config: ConfigService) => {
-    //     return {
-    //       type: 'sqlite',
-    //       database: config.get<string>('DB_NAME'),
-    //       synchronize: true,
-    //       entities: [User, Report]
-    //     }
-    //   }
-    // }),
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'db.sqlite',
-    //   entities: [User, Report],
-    //   synchronize: true,
-    // }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Report]
+        }
+      }
+    }),
     UsersModule,
     ReportsModule
   ],
