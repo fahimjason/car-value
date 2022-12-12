f2637babd7a077581cf78b9a1183bca9043fd338
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2022-12-12 10:36:53 +0600

    Implement env-specific database config

---
+++ ormconfig.js
@@ -0,0 +1,27 @@
+var dbConfig = {
+    synchronize: false,
+};
+
+switch (process.env.NODE_ENV) {
+    case 'development':
+        Object.assign(dbConfig, {
+            type: 'sqlite',
+            database: 'db.sqlite',
+            entities: ['**/*.entity.js']
+        });
+        break;
+    case 'test':
+        Object.assign(dbConfig, {
+            type: 'sqlite',
+            database: 'test.sqlite',
+            entities: ['**/*.entity.ts']
+        });
+        break;
+    case 'production':
+        break;
+
+    default:
+        throw new Error('Unknown environment');
+}
+
+module.exports = dbConfig;
